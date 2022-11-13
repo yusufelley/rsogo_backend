8e879c3568efb26814f54d1f7387abd7df9cdc93
mounirkanane <71102660+MounirKanane@users.noreply.github.com>
2022-11-13 03:37:01 -0500

    image

--- Models/event.js
+++ Models/event.js
@@ -16,6 +16,10 @@ const eventSchema = new Schema(
       type: String,
       required: true,
     },
+    image: {
+      type: String,
+      required: true,
+    },
     location: {
       type: String,
       required: true,
